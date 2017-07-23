d94ed4c4b6577c5dde921b3dcea198522417a952
YuierWZ <995059531@qq.com>
2017-07-23 12:34:26 +0800

    基础

--- index.php
+++ index.php
@@ -1,4 +1,54 @@
 <?php 
-echo "php";
-echo "my first php script";
+$x=5;
+$y=10;
+$cars=array("vovle","bwm","benzs");
+echo "php\n ";
+echo "my first php script\n";
+function myTest(){
+    static $y=10;
+    echo "Test variables inside the function:\n";//源代码中实现换行
+    echo $y;
+    $y++;
+    echo "\n";
+    echo "</br>";//在网页中实现了换行
+ }
+ myTest();
+ echo "Test variables outside the function:\n";
+ echo $x;
+ 
+ function myTest2(){
+//     global $x,$y;
+//     $y=$x+$y;
+$GLOBALS['y']=$GLOBALS['x']+$GLOBALS['y'];
+    echo "\n";
+
+ }
+ myTest2();
+ echo $y;
+ echo "\n";
+ myTest();
+ myTest();
+ myTest();
+echo "\n";
+ echo $cars[0];
+ echo "\n";
+ print "I will work hard\n";
+ print_r($cars);
+ echo "\n";
+ var_dump($x);
+ echo "\n";
+ $xe=0x11;
+ var_dump($xe);
+ echo "\n";
+ var_dump($cars);
+ echo "\n";
+ define(AE, "this is a define");
+ echo AE;
+ echo "\n";
+ $txt1="string ";
+ $txt2="is important";
+ echo $txt1." ".$txt2."\n";
+ echo strlen($txt1);
+ echo "\n";
+ echo strpos($txt1, r)."\n";
  ?>
